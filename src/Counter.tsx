4fbe7c3a4096604bbb93d18b37cca5d08553d9f8
matsuko <atsuko_mouri@heartcore.co.jp>
2024-02-27 11:36:49 +0900

    add useref

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect,useRef,useState} from 'react';
+//コンポーネントがレンダリングされたら丸っと関数を実行してくれる機能→useEffect
 
 // type MySetStateAction = number | ((prevState: number) => number);
 // type Dispatch = (value: MySetStateAction) => void;
 
+// const array: Array<number> = [1,2,3];
+// const readonlyArray: ReadonlyArray<number> = [1,2,3];
+// array[0] = 11
+// readonlyArray[0] = 11
 
 const Counter: React.FunctionComponent<{}> = () => {
 	const initialValue: any = 0;
@@ -15,11 +20,18 @@ const Counter: React.FunctionComponent<{}> = () => {
 	const decrement = () => {
 		setValue( prevState => prevState - 1);
 	}
+
+	const renderTimes = useRef<number>(0)
+	useEffect(()=>{
+		renderTimes.current = renderTimes.current +1
+	})
+
 	return (
 		<div>
 			<div>value: {value}</div>
 			<button onClick={increment}>+1</button>
 			<button onClick={decrement}>-1</button>
+			<div>This component was rerendered{renderTimes.current} times</div>
 		</div>
 	);
 };
