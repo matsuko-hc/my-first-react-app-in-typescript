eb452ca7784da12a03f073b2cb0efc2b62bbab8e
matsuko <atsuko_mouri@heartcore.co.jp>
2024-02-27 09:33:30 +0900

    function-components初級編

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+//インターフェースで型を指定してあげるとのちに再利用もできる
+interface AppProps {
+  message: string;
+}
+
+//厳密に型制約することで、予期せぬデータの混入を防ぐことができる
+const App = ({message}: AppProps) => {
+  // console.log(props)
+  // const {message} = props
+  return <div>{ message }</div>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// type  Foo =  JSX.IntrinsicAttributes;
+
+//renderメソッドは第二引数を第一引数で置き換えるメソッド
+ReactDOM.render(<App message="メッセージ"/>, document.getElementById('root'));
